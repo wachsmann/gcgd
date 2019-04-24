6ecfbd5aee5c5a53bd6d8926b49832d66d49cdce
Leonardo Dalbosco <leodalbosco@hotmail.com>
2019-04-23 21:58:33 -0300

    Alteração das tela de cadastro e tela e login

--- corgis-react/src/views/Register/RegisterView.js
+++ corgis-react/src/views/Register/RegisterView.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { userActions } from '../../_actions';
 import { connect } from 'react-redux';
 import './index.css'
+import {Col} from "react-bootstrap";
 
 class RegisterView extends React.Component {
     constructor(props) {
@@ -49,51 +50,53 @@ class RegisterView extends React.Component {
 
         const { user, submitted } = this.state;
         return (
-            <div className="col-md-6 col-md-offset-3">
-                <h2>Cadastro</h2>
-                { alert && alert.message ?
-                    <div className={`alert ${alert.type}`}>{alert.message}</div>
-                    :<div/>
-                }
-                <form name="form" onSubmit={this.handleSubmit}>
-                    <div className={'form-group' + (submitted && !user.name ? ' has-error' : '')}>
-                        <label htmlFor="name">Nome</label>
-                        <input type="text" className="form-control" name="name" value={user.name} onChange={this.handleChange} />
-                        {submitted && !user.name &&
-                            <div className="help-block">Nome é obrigatório!</div>
-                        }
-                    </div>
-                    <div className={'form-group' + (submitted && !user.email ? ' has-error' : '')}>
-                        <label htmlFor="email">E-mail</label>
-                        <input type="text" className="form-control" name="email"
-                         value={user.email} onChange={this.handleChange} />
-                        {submitted && !user.email &&
-                            <div className="help-block">E-mail é obrigatório!</div>
-                        }
-                    </div>
-                    <div className={'form-group' + (submitted && !user.phone ? ' has-error' : '')}>
-                        <label htmlFor="phone">Telefone</label>
-                        <input type="text" className="form-control" name="phone"
-                         value={user.phone} onChange={this.handleChange} />
-                        {submitted && !user.phone &&
-                            <div className="help-block">Telefone é obrigatório!</div>
-                        }
-                    </div>                    
-                    <div className={'form-group' + (submitted && !user.password ? ' has-error' : '')}>
-                        <label htmlFor="password">Senha</label>
-                        <input type="password"   className="form-control" name="password"
-                         value={user.password} onChange={this.handleChange} />
-                        {submitted && !user.password &&
-                            <div className="help-block">Senha é obrigatória!</div>
-                        }
-                    </div>
-                    <div className="form-group">
-                        <button className="btn btn-primary">Cadastrar</button>
-                        {registering && 
-                            <img alt="loader" src="data:image/gif;base64,R0lGODlhEAAQAPIAAP///wAAAMLCwkJCQgAAAGJiYoKCgpKSkiH/C05FVFNDQVBFMi4wAwEAAAAh/hpDcmVhdGVkIHdpdGggYWpheGxvYWQuaW5mbwAh+QQJCgAAACwAAAAAEAAQAAADMwi63P4wyklrE2MIOggZnAdOmGYJRbExwroUmcG2LmDEwnHQLVsYOd2mBzkYDAdKa+dIAAAh+QQJCgAAACwAAAAAEAAQAAADNAi63P5OjCEgG4QMu7DmikRxQlFUYDEZIGBMRVsaqHwctXXf7WEYB4Ag1xjihkMZsiUkKhIAIfkECQoAAAAsAAAAABAAEAAAAzYIujIjK8pByJDMlFYvBoVjHA70GU7xSUJhmKtwHPAKzLO9HMaoKwJZ7Rf8AYPDDzKpZBqfvwQAIfkECQoAAAAsAAAAABAAEAAAAzMIumIlK8oyhpHsnFZfhYumCYUhDAQxRIdhHBGqRoKw0R8DYlJd8z0fMDgsGo/IpHI5TAAAIfkECQoAAAAsAAAAABAAEAAAAzIIunInK0rnZBTwGPNMgQwmdsNgXGJUlIWEuR5oWUIpz8pAEAMe6TwfwyYsGo/IpFKSAAAh+QQJCgAAACwAAAAAEAAQAAADMwi6IMKQORfjdOe82p4wGccc4CEuQradylesojEMBgsUc2G7sDX3lQGBMLAJibufbSlKAAAh+QQJCgAAACwAAAAAEAAQAAADMgi63P7wCRHZnFVdmgHu2nFwlWCI3WGc3TSWhUFGxTAUkGCbtgENBMJAEJsxgMLWzpEAACH5BAkKAAAALAAAAAAQABAAAAMyCLrc/jDKSatlQtScKdceCAjDII7HcQ4EMTCpyrCuUBjCYRgHVtqlAiB1YhiCnlsRkAAAOwAAAAAAAAAAAA==" />
-                        }
-                    </div>
-                </form>
+            <div className="card-box">
+                <Col xs={12} md={12}>
+                    <h1>Cadastro</h1>
+                    { alert && alert.message ?
+                        <div className={`alert ${alert.type}`}>{alert.message}</div>
+                        :<div/>
+                    }
+                    <form name="form" onSubmit={this.handleSubmit}>
+                        <div className={'form-group' + (submitted && !user.name ? ' has-error' : '')}>
+                            <label htmlFor="name">Nome</label>
+                            <input type="text" className="form-control" name="name" value={user.name} onChange={this.handleChange} />
+                            {submitted && !user.name &&
+                                <div className="help-block">Nome é obrigatório!</div>
+                            }
+                        </div>
+                        <div className={'form-group' + (submitted && !user.email ? ' has-error' : '')}>
+                            <label htmlFor="email">E-mail</label>
+                            <input type="text" className="form-control" name="email"
+                             value={user.email} onChange={this.handleChange} />
+                            {submitted && !user.email &&
+                                <div className="help-block">E-mail é obrigatório!</div>
+                            }
+                        </div>
+                        <div className={'form-group' + (submitted && !user.phone ? ' has-error' : '')}>
+                            <label htmlFor="phone">Telefone</label>
+                            <input type="text" className="form-control" name="phone"
+                             value={user.phone} onChange={this.handleChange} />
+                            {submitted && !user.phone &&
+                                <div className="help-block">Telefone é obrigatório!</div>
+                            }
+                        </div>
+                        <div className={'form-group' + (submitted && !user.password ? ' has-error' : '')}>
+                            <label htmlFor="password">Senha</label>
+                            <input type="password"   className="form-control" name="password"
+                             value={user.password} onChange={this.handleChange} />
+                            {submitted && !user.password &&
+                                <div className="help-block">Senha é obrigatória!</div>
+                            }
+                        </div>
+                        <div className="form-group buttonSubmit">
+                            <button className="btn btn-primary">Cadastrar</button>
+                            {registering &&
+                                <img alt="loader" src="data:image/gif;base64,R0lGODlhEAAQAPIAAP///wAAAMLCwkJCQgAAAGJiYoKCgpKSkiH/C05FVFNDQVBFMi4wAwEAAAAh/hpDcmVhdGVkIHdpdGggYWpheGxvYWQuaW5mbwAh+QQJCgAAACwAAAAAEAAQAAADMwi63P4wyklrE2MIOggZnAdOmGYJRbExwroUmcG2LmDEwnHQLVsYOd2mBzkYDAdKa+dIAAAh+QQJCgAAACwAAAAAEAAQAAADNAi63P5OjCEgG4QMu7DmikRxQlFUYDEZIGBMRVsaqHwctXXf7WEYB4Ag1xjihkMZsiUkKhIAIfkECQoAAAAsAAAAABAAEAAAAzYIujIjK8pByJDMlFYvBoVjHA70GU7xSUJhmKtwHPAKzLO9HMaoKwJZ7Rf8AYPDDzKpZBqfvwQAIfkECQoAAAAsAAAAABAAEAAAAzMIumIlK8oyhpHsnFZfhYumCYUhDAQxRIdhHBGqRoKw0R8DYlJd8z0fMDgsGo/IpHI5TAAAIfkECQoAAAAsAAAAABAAEAAAAzIIunInK0rnZBTwGPNMgQwmdsNgXGJUlIWEuR5oWUIpz8pAEAMe6TwfwyYsGo/IpFKSAAAh+QQJCgAAACwAAAAAEAAQAAADMwi6IMKQORfjdOe82p4wGccc4CEuQradylesojEMBgsUc2G7sDX3lQGBMLAJibufbSlKAAAh+QQJCgAAACwAAAAAEAAQAAADMgi63P7wCRHZnFVdmgHu2nFwlWCI3WGc3TSWhUFGxTAUkGCbtgENBMJAEJsxgMLWzpEAACH5BAkKAAAALAAAAAAQABAAAAMyCLrc/jDKSatlQtScKdceCAjDII7HcQ4EMTCpyrCuUBjCYRgHVtqlAiB1YhiCnlsRkAAAOwAAAAAAAAAAAA==" />
+                            }
+                        </div>
+                    </form>
+                </Col>
             </div>
         );
   }
